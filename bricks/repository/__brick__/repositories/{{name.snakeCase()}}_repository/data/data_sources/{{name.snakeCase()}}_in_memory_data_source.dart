fe9348f75dd362bc5ef67eb8c47dae8710464434
Marco Trivisonno <marco.trivi2@gmail.com>
2024-07-07 19:20:30 +0200

    ref: improves repository brick
    Closes #8

--- bricks/repository/__brick__/repositories/{{name.snakeCase()}}_repository/data/data_sources/{{name.snakeCase()}}_in_memory_data_source.dart
+++
@@ -1,6 +0,0 @@
-import 'package:a2f_sdk/a2f_sdk.dart';
-
-import '../../{{name.snakeCase()}}_repository.dart';
-
-final class {{name.pascalCase()}}InMemoryDataSource
-    extends InMemoryDataSource<{{entity.pascalCase()}}Id, {{entity.pascalCase()}}Entity> implements {{name.pascalCase()}}DataSource {}
